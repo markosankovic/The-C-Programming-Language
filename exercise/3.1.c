baf4b4c13e28c5f0826ffd9a21229c0c4b0f154f
Marko Sanković <sankovic.marko@gmail.com>
2015-10-02 09:58:04 +0200

    Exercise 3.1

---
+++ exercise/3.1.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/* binsearch: find x in v[0] <= v[1] <= ... <= v[n-1] */
+int binsearch(int x, int v[], int n) {
+  int low, high, mid;
+  low = 0;
+  high = n - 1;
+  while (low <= high) {
+    mid = (low+high)/2;
+    if (x < v[mid])
+      high = mid + 1;
+    else if (x  > v[mid])
+      low = mid + 1;
+    else /* found match */
+      return mid;
+  }
+  return -1; /* no match */
+}
+
+int main() {
+
+  int n = 9;
+  int v[9] = {3,4,5,6,7,8,9,10,11};
+
+  printf("%d", binsearch(5, v, n));
+
+  return 0;
+}
